75d5b2e3a9a8293ad13dd9a7c781a4a8c8aedff9
dim-on <dmitriy.s.panov@gmail.com>
2016-06-03 16:59:34 +0300

    in progress yet

---
+++ homework_26.05/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Резюме</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<section>
+    <!--Name+Basic Info+Photo-->
+    <div>
+        <div id="basic-info">
+            <h2 id="name">Панов Дмитрий Сергеевич</h2>
+            <ul id="basic-info-list">
+                <li type="none">Дата рождения: 31.01.1982</li>
+                <li type="none">Место жительства: Киев</li>
+                <li type="none">Телефон: +38.050.988.08.89</li>
+                <li type="none">e-mail: <a href="mailto:dmitriy.s.panov@gmail.com">dmitriy.s.panov@gmail.com</a></li>
+            </ul>
+        </div>
+        <div id="cv_photo">
+            <img src="images/cv_photo.jpg" alt="Panov">
+        </div>
+    </div>
+    <!--Education+Jobs-->
+    <div>
+        <ul id="list-of-jobs">
+            <li>Образование</li>
+            <div class="duration">
+                <p>Сентябрь 1999 – Июль 2004</p>
+            </div>
+            <div>
+                <p>Донецкий национальный технический университет <br>
+                    Факультет: Электротехнический, Французский технический <br>
+                    Специальность: Электрификация и автоматизация промышленных установок и электропривод
+                </p>
+            </div>
+            <li>Опыт работы</li>
+<?php
+include_once 'variables.php';
+
+        foreach ($experience as $key=>$value) {
+            echo $key;
+            echo $value;
+        }
+
+//echo $duration1.'<br>'.$description1.'<br>'.$duration2.'<br>'.$description2.'<br>'.$duration3.'<br>'.$description3.'<br>';
+//echo $duration4.'<br>'.$description4.'<br>'.$duration5.'<br>'.$description5.'<br>'.$duration6.'<br>'.$description6.'<br>';
+?>
+<!--Additional Info -->
+                <li>Профессиональные навыки:</li>
+                <p>
+                <ul class="secondary-list">
+                    <li>Уверенный пользователь ПК (AutoCAD, Windows, MS Office, 1C …)</li>
+                    <li>Водительское удостоверение категории «В»</li>
+                </ul>
+                </p>
+                <li>Языки:</li>
+                <p>
+                <ul class="secondary-list">
+                    <li>Французский - уверенный (DELF 1er degré, DFA 1er degré);</li>
+                    <li>Английский – базовый (А2), технический английский</li>
+                </ul>
+                </p>
+                <li>Личные качества:</li>
+                <p style="text-align: justify">Организованность, аналитические способности, ответственность, внимание к деталям,
+                    коммуникабельность, обучаемость,  организаторские способности, стремление к личностному и
+                    профессиональному росту.
+                </p>
+            </ul>
+        </div>
+    </section>
+</body>
+</html>
